d468abea7de1f95fa224c4606839670935736970
Gustavo Claudio Karl Couto <gustavokarlcouto@gmail.com>
2020-03-16 00:46:30 -0300

    First commit

---
+++ precache-manifest.87e5b71b693b62b44e33bd21c3345d07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef70b2027bd9a5860fa3cf1a88de66e7",
+    "url": "/web-des-deploy/index.html"
+  },
+  {
+    "revision": "5510334b194e75bc7fa8",
+    "url": "/web-des-deploy/static/css/main.d7936dd0.chunk.css"
+  },
+  {
+    "revision": "c439af14527738919167",
+    "url": "/web-des-deploy/static/js/2.222180a1.chunk.js"
+  },
+  {
+    "revision": "1467b5aa8446b5dbef93be7aeba56463",
+    "url": "/web-des-deploy/static/js/2.222180a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5510334b194e75bc7fa8",
+    "url": "/web-des-deploy/static/js/main.4c308f67.chunk.js"
+  },
+  {
+    "revision": "4075a01dc1969eadc444",
+    "url": "/web-des-deploy/static/js/runtime-main.f1759e59.js"
+  }
+]);
